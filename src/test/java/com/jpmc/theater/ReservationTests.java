f78f8b99702c740aca2167faa46ae075bb225a1a
danjer9 <103093841+danjer9@users.noreply.github.com>
2022-08-22 23:43:34 -0400

    Update ReservationTests.java
    Specified it tests total fees before discounts.

--- src/test/java/com/jpmc/theater/ReservationTests.java
+++ src/test/java/com/jpmc/theater/ReservationTests.java
@@ -10,13 +10,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ReservationTests {
 
     @Test
-    void totalFee() {
+    void totalFeeBeforeDiscount() {
         var customer = new Customer("John Doe", "unused-id");
         var showing = new Showing(
                 new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 12.5, 1),
                 1,
                 LocalDateTime.now()
         );
-        assertEquals(37.5, new Reservation(showing, 3).totalFee());
+        assertEquals(37.5, new Reservation(customer, showing, 3).totalFee());
     }
+
 }
