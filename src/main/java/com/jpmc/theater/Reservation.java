713883dd7fe4455bc67b53220cab4bee160928c2
danjer9 <103093841+danjer9@users.noreply.github.com>
2022-08-22 23:52:44 -0400

    Update Reservation.java

--- src/main/java/com/jpmc/theater/Reservation.java
+++ src/main/java/com/jpmc/theater/Reservation.java
@@ -1,10 +1,12 @@
 package com.jpmc.theater;
 
 public class Reservation {
-    public final Showing showing;
+    private final Customer customer;
+    private final Showing showing;
     private final int audienceCount;
 
-    public Reservation(Showing showing, int audienceCount) {
+    public Reservation(Customer customer, Showing showing, int audienceCount) {
+        this.customer = customer;
         this.showing = showing;
         this.audienceCount = audienceCount;
     }
